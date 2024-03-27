deb17da0e9eb25fc7ee53b75118815d7c08afe91
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-03-27 14:34:25 +0100

    mvp

--- extension.js
+++ extension.js
@@ -1,36 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require('vscode')
+const validator = require('validator')
+const md5 = require('md5')
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  let disposable = vscode.commands.registerCommand('bloom-env.getHash', async function () {
+    const rawEmail = await vscode.window.showInputBox({
+      prompt: "Enter your email, please."
+    })
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bloom-env" is now active!');
+    if (rawEmail === null) {
+      vscode.window.showInformationMessage('No email was entered.')
+      return
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bloom-env.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    const email = rawEmail.trim()
+    const isEmailValid = validator.isEmail(email)
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from bloom-env!');
-	});
+    if (email && isEmailValid) {
+      vscode.window.showInformationMessage(`Your magic code: ${md5(email).slice(0, 10)}`)
+    } else {
+      vscode.window.showInformationMessage('No valid email was entered.')
+    }
+  })
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate,
 }
